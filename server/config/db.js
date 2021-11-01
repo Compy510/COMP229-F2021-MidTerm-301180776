25004f8577dd041c157070f16cfcba4453940920
Compy510 <90357429+Compy510@users.noreply.github.com>
2021-11-01 09:58:19 -0400

    connected to booklist to mongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://arshy:8qzSRzWPiD8vwrh1@comp229-m1.lxq5b.mongodb.net/library?retryWrites=true&w=majority"
 };
